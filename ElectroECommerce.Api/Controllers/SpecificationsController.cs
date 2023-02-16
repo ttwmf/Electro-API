4c558fad7bbc64070294af54ac2f9972842bb8a8
HHV01 <hoanghongvan2001@gmail.com>
2023-02-17 00:31:10 +0700

    hong-van/api-CRUD-specifications

---
+++ ElectroECommerce.Api/Controllers/SpecificationsController.cs
@@ -0,0 +1,100 @@
+﻿using ElectroECommerce.Application.Contracts;
+using ElectroECommerce.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElectroECommerce.Api.Controllers
+{
+    [ApiController]
+    [Route("api/specifications")]
+    public class SpecificationsController : ControllerBase
+    {
+        private readonly ISpecificationsService _specificationsService;
+        public SpecificationsController(ISpecificationsService specificationsService)
+        {
+            _specificationsService = specificationsService;
+        }
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<IEnumerable<Specifications>>> GetSpecificationsAsync()
+        {
+            var result = await _specificationsService.GetSpecificationsAsync();
+            return Ok(result);
+        }
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Specifications>> GetSpecificationByIdAsync(int id)
+        {
+            try
+            {
+                var result = await _specificationsService.GetSpecificationsByIdAsync(id);
+                if (result == null)
+                    return NotFound("specification no existed");
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from server");
+            }
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<Specifications>> CreateSpecificationsAsync(Specifications specifications)
+        {
+            try
+            {
+                var result = await _specificationsService.CreateSpecificationsAsync(specifications);
+                if (result == null)
+                    return BadRequest("Object specification is null");
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from server");
+            }
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<ActionResult<Specifications>> UpdateSpecificationsAsync(int id, Specifications specifications)
+        {
+            try
+            {
+                Specifications specIsvalid = await _specificationsService.GetSpecificationsByIdAsync(id);
+                if (specIsvalid == null)
+                {
+                    return BadRequest("Id = " + id + " not found in Database");
+                }
+                else
+                {
+                    specifications.Id = specIsvalid.Id;
+                    var result = await _specificationsService.UpdateSpecificationsAsync(specifications);
+                    return Ok(result);
+                }
+
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from server");
+            }
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<ActionResult> DeleteSpecificationsAsync(int id)
+        {
+            try
+            {
+                var specValid = await GetSpecificationByIdAsync(id);
+                if (specValid == null)
+                    return NotFound("Specification no existed");
+                await _specificationsService.DeleteSpecificationsAsync(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
---
+++ ElectroECommerce.Infrastructure/Repositories/SpecificationsRepository.cs
@@ -0,0 +1,17 @@
+﻿using ElectroECommerce.Application.IRepositories;
+using ElectroECommerce.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectroECommerce.Infrastructure.Repositories
+{
+    public class SpecificationsRepository : GenericRepository<Specifications>, ISpecificationsRepository
+    {
+        public SpecificationsRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
